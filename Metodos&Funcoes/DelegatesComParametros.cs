f39571eba6e9df341c9f1b9ce93be1e7ddd74dec
JoseGhu <69773420+JoseGhu@users.noreply.github.com>
2025-04-21 18:22:36 -0300

    Feito por upload

---
+++ Metodos&Funcoes/DelegatesComParametros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace CursoCSharp.Metodos_Funcoes
+{
+    class DelegatesComParametros
+    {
+        public delegate int Operacao(int x, int  y);
+
+        public static int Soma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static string Calculadora(Operacao op, int x, int y)
+        {
+            var resultado = op(x, y);
+            return "Resultado: " + resultado;
+        }
+
+        public static void Executar()
+        {
+            Operacao Subtracao = (int x, int y) => x - y;
+            Console.WriteLine(Calculadora(Subtracao, 3, 2));
+            Console.WriteLine(Calculadora(Soma, 3, 2));
+        }
+    }
+}
---
+++ Metodos&Funcoes/Lambda.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace CursoCSharp.Metodos_Funcoes
+{
+    class Lambda
+    {
+        public static void Executar()
+        {
+            Action algoNoConsole = () => 
+            {
+                Console.WriteLine("Lambda com C#! ");
+            };
+
+            algoNoConsole();
+
+            Func<int> jogarDado = () => 
+            {
+                Random ramdom = new Random();
+                return ramdom.Next(1, 7);
+            };
+
+            Console.WriteLine(jogarDado());
+
+            Func<int, string> conversorHex = numero => numero.ToString("X");
+            Console.WriteLine(conversorHex(1234));
+
+            Func<int, int, int, string> formatarData = (dia, mes, ano) =>
+                String.Format("{0:D2}/{1:D2}/{2:D4}", dia, mes, ano);
+            Console.WriteLine(formatarData(4, 4, 1999));
+
+        }
+    }
+}
